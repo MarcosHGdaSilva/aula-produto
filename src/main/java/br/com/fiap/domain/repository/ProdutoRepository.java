947897af95854616be5f0ea8b1dbaab010e11ae7
logonrmlocal <henriquegarrido1998@gmail.com>
2023-08-04 09:35:31 -0300

    criado o service e view de produto

--- src/main/java/br/com/fiap/domain/repository/ProdutoRepository.java
+++ src/main/java/br/com/fiap/domain/repository/ProdutoRepository.java
@@ -6,6 +6,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class ProdutoRepository {
 
@@ -48,4 +49,18 @@ public class ProdutoRepository {
         return retorno;
     }
 
+    public static Produto persist(Produto p) {
+        if(Objects.isNull(p.getNome())){
+            return null;
+
+        }
+        if(Objects.isNull(p.getDescricao())){
+            return null;
+        }
+        if(Objects.isNull(p.getValor())){
+            return null;
+        }
+        return ProdutoRepository.persist(p);
+
+    }
 }
